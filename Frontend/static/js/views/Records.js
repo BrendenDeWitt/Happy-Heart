ddb7f880c5663b624acb4c7b210b773f28fb9a8e
BrendenDeWitt <70327934+BrendenDeWitt@users.noreply.github.com>
2022-04-29 02:04:24 -0500

    Update Records.js
    Updated Records and removed the table

--- Frontend/static/js/views/Records.js
+++ Frontend/static/js/views/Records.js
@@ -9,8 +9,8 @@ export default class extends AbstractView {
     async getHtml() {
         return `
            <!DOCTYPE html>
-            <html lang="en">
-            <head>
+			<html lang="en">
+			<head>
                 <meta charset="UTF-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
                 <title>Records</title>
@@ -18,25 +18,28 @@ export default class extends AbstractView {
                 <script src="jquery.js"></script>
                 <script type="module" src="Frontend/static/js/views/home.js"></script>
             </head>
-            <body>
-                <h1>Records</h1>
-                <div class="record_cont">
-                    <form class="record_form">
-                    <button class="share_button">Share</button>
-                        <table id="Records_Table">
-                            <thead>
-                                <tr>
-                                    <th>Date</th>
-                                    <th>Systolic pressure</th>
-                                    <th>Diastolic pressure</th>
-                                </tr>    
-                            </thead>
-                            <tbody id="recordsTable"></tbody>
-                        </table>
-                    </form>
-                </div>
-            </body>
-            </html>
+			<body>
+				<h1>Records</h1>
+				<div class="record_cont">
+					<button class="display_button" onclick="displayAll()">Display All Results</button>
+				</div>
+				<p id="allResults"></p>
+				<a href = "https://www.facebook.com/">
+				<button id = "facebook_icon" onclick="fileShare()"></button>
+				</a>
+				<p>Share data with Facebook</p>
+				<br>
+				<a href = "https://www.gmail.com">
+				<button id = "gmail_icon" ></button>
+				</a>
+				<p>Share data with Gmail</p>
+				<br>
+				<a href = "https://login.microsoftonline.com">
+				<button id = "office365_icon"></button>
+				</a>
+				<p>Share data with Office365</p>
+			</body>
+			</html>
         `;
     }
 }

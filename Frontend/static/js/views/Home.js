5b46b9837a63bfc0f5c2a36845f8490cf0e29e23
BrendenDeWitt <70327934+BrendenDeWitt@users.noreply.github.com>
2022-04-29 02:03:31 -0500

    Update Home.js
    Updated home.js

--- Frontend/static/js/views/Home.js
+++ Frontend/static/js/views/Home.js
@@ -11,26 +11,22 @@ export default class extends AbstractView {
 <!DOCTYPE html>
               
                 <h1 class="home_title">Happy Heart ♥️</h1>
-                <div class="home_cont">
-                    <form class="home_form" id="home">
-                        <div class="home_message">
-                            <p>Please enter your current Systolic and Diastolic pressure.</p>
-                        </div>
-                        <div class="home_input-group">
-                            <input class="home_input" id="sysPress" type="text" placeholder="Systolic pressure"> 
-                        </div>
-                        <div class="home_input-group">
-                            <input class="home_input" id="diaPress" type="text" placeholder="Diastolic pressure">
-                        </div>
-                        <button class="home_button" id="home_button" type="submit">Enter</button>
-                        <br>
-                    </form>
-                </div>
-                <div class="home_results">
-                    <form class="result_form form_hidden">
-                        <p>Results will be displayed here</p>
-                    </form>
-                </div>
+				<div class="home_cont">
+					<div class="home_message">
+						<p>Please enter your current Systolic and Diastolic pressure.</p>
+					</div>
+					<div class="home_input-group">
+						<input class="home_input" id="sysPress" type="text" placeholder="Systolic pressure">
+					</div>
+					<div class="home_input-group">
+						<input class="home_input" id="diaPress" type="text" placeholder="Diastolic pressure">
+					</div>
+					<button class="home_button" id="home_button" type = "submit" onclick="showResult()">Enter</button>
+					<br>
+				</div>
+				<div class="home_results">
+					<p id="result"></p>
+				</div>
         `;
     }
 };

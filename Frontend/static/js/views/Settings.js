6388fdab9ac48c9cac04b7c0e835bb26dcd0ec5c
BrendenDeWitt <70327934+BrendenDeWitt@users.noreply.github.com>
2022-04-29 02:05:00 -0500

    Update Settings.js
    Added a delete records button

--- Frontend/static/js/views/Settings.js
+++ Frontend/static/js/views/Settings.js
@@ -9,6 +9,7 @@ export default class extends AbstractView {
     async getHtml() {
         return `
             <h1>Settings</h1>
+            <button class="settings_button" id="settings_button" type="submit" onclick="deleteAll()">Delete Records</button>
             <p></p>
         `;
     }
